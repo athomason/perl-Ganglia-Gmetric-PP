c997024ecb6090980f4f207dc809e75bd98ed336
Adam Thomason <ad@mthomason.net>
2009-09-10 18:14:48 -0700

    Checking in changes prior to tagging of version 0.92.  Changelog diff is:
    diff --git a/Changes b/Changes
    index d00735f..239b552 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,7 @@
     Revision history for Perl extension Ganglia::Gmetric::PP.
    
    +0.92  Thu Sep 10 18:14:36 PDT 2009
    +        - Add docs and comments to aggregator
     0.91  Thu Sep 10 17:54:40 PDT 2009
             - Support Danga::Socket as fallback from AnyEvent
     0.90  Thu Sep 10 17:36:50 PDT 2009

--- bin/gmetric-aggregator.pl
+++ bin/gmetric-aggregator.pl
@@ -6,41 +6,63 @@ use warnings;
 use Data::Dumper ();
 use Ganglia::Gmetric::PP ':all';
 use Getopt::Long 'GetOptions';
-use Time::HiRes ();
+use Time::HiRes 'time';
 
+sub usage {
+    (my $me = $0) =~ s,.*/,,;
+    my $error = shift;
+    print "Error: $error\n\n" if $error;
+    print <<EOU;
+$me $Ganglia::Gmetric::PP::VERSION
+
+Usage: $me [OPTIONS]...
+
+  -h, --remote-host=STRING  Remote host where gmond is running (required)
+  -p, --remote-port=INT     Remote port where gmond is running. Default 8649
+  -H, --listen-host=STRING  Local interface to listen on. Default 0.0.0.0
+  -P, --listen-port=INT     Local UDP port to listen on. Default 18649
+  -n, --period=INT          Time period in seconds between aggregations. Default 60
+  -s, --suffix=STRING       Suffix to append to gmetric units. Default "/s"
+  -g, --debug               Display debugging output
+  --help                    Print help and exit
+EOU
+    exit 1;
+}
+
+Getopt::Long::Configure('no_ignore_case');
 GetOptions(
-    'remote-host=s' => \(my $remote_host),
-    'remote-port=s' => \(my $remote_port = 8649),
-    'listen-host=i' => \(my $listen_host = '0.0.0.0'),
-    'listen-port=i' => \(my $listen_port = 18649),
-    'period=i'      => \(my $period = 60),
-    'suffix=s'      => \(my $units_suffix = '/s'),
-    'g|debug'       => \(my $debug),
-    'help!'         => \(my $help),
-);
+    'h|remote-host=s'   => \(my $remote_host),
+    'p|remote-port=i'   => \(my $remote_port    = 8649),
+    'H|listen-host=s'   => \(my $listen_host    = '0.0.0.0'),
+    'P|listen-port=i'   => \(my $listen_port    = 18649),
+    'n|period=i'        => \(my $period         = 60),
+    's|suffix=s'        => \(my $units_suffix   = '/s'),
+    'g|debug'           => \(my $debug),
+    'help!'             => \(my $help),
+) || usage;
+
+usage if $help;
+usage('--remote-host needed') unless defined $remote_host;
 
 my $use_anyevent;
 if (eval "use AnyEvent; 1") {
     $debug && warn "Using AnyEvent\n";
     $use_anyevent = 1;
-    *time = sub { AnyEvent->now };
 }
 elsif (eval "use Danga::Socket; 1") {
     $debug && warn "Using Danga::Socket\n";
     $use_anyevent = 0;
-    *time = \&Time::Hires::time;
 }
 else {
     die "need either AnyEvent or Danga::Socket module";
 }
-
-die "--remote-host needed" unless defined $remote_host;
 
 my $emitter = Ganglia::Gmetric::PP->new(
     host => $remote_host,
     port => $remote_port,
 );
 
+# udp server socket
 my $listener = IO::Socket::INET->new(
     Proto       => 'udp',
     LocalHost   => $listen_host,
@@ -55,12 +77,20 @@ my %allowed_types = map {$_ => 1} qw/ double float int16 int32 uint16 uint32 /;
 my %metric_aggregates;
 my %metric_templates;
 sub handle {
+    # udp packet has been received
     return unless $listener->recv(my $buf, 1 << 14);
+
+    # parse and validate gmetric packet
     my @sample = $emitter->parse($buf);
     return unless $allowed_types{ $sample[METRIC_INDEX_TYPE] };
-    $debug && warn Data::Dumper->Dump([\@sample], ['sample']);
+
+    # aggregate sums on the fly
     $metric_aggregates{ $sample[METRIC_INDEX_NAME] } += $sample[METRIC_INDEX_VALUE];
+
+    # keep an example copy of this metric to re-emit with aggregated values
     $metric_templates{ $sample[METRIC_INDEX_NAME] } ||= \@sample;
+
+    $debug && warn Data::Dumper->Dump([\@sample], ['sample']);
 }
 my $watcher;
 if ($use_anyevent) {
@@ -71,26 +101,31 @@ else {
 }
 
 # periodically aggregate collected samples and re-emit to target gmond
-my %metrics_seen;
 my $last_time = time;
 my $timer;
 sub aggregator {
     my $time = time;
     my $measured_period = $time - $last_time;
     $debug && warn "Aggregating at $time ($measured_period elapsed)\n";
-    $metrics_seen{$_} ||= 1 for keys %metric_aggregates;
-    for my $metric (keys %metrics_seen) {
+
+    # emit for any metric seen before, even if it wasn't seen in the last period
+    for my $metric (keys %metric_templates) {
         my @aggregate = @{ $metric_templates{$metric} };
+
+        # aggregated value is rate of metric over last period
         $aggregate[METRIC_INDEX_VALUE] = ($metric_aggregates{$metric}||0) / ($measured_period||1);
         $aggregate[METRIC_INDEX_VALUE] = int($aggregate[METRIC_INDEX_VALUE])
             if $aggregate[METRIC_INDEX_TYPE] =~ /int/;
+
         $aggregate[METRIC_INDEX_UNITS] .= $units_suffix;
         $aggregate[METRIC_INDEX_TMAX] = $period;
+
         $emitter->gsend(@aggregate);
         $debug && warn Data::Dumper->Dump([\@aggregate], ["${metric}_aggregated"]);
     }
     %metric_aggregates = ();
-    $last_time = time;
+
+    $last_time = $time;
     if ($use_anyevent) {
         $timer = AnyEvent->timer(after => $period, cb => \&aggregator);
     }
@@ -99,6 +134,7 @@ sub aggregator {
     }
 }
 
+# run event loop
 if ($use_anyevent) {
     $timer = AnyEvent->timer(after => $period, cb => \&aggregator);
     AnyEvent->condvar->recv;
