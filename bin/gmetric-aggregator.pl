1c1e22b94ea1cb0f0e62832d8733780856b4f6dd
athomason <athomason@athomason-many.(none)>
2009-09-10 17:32:37 -0700

    Bunch of small improvements.
    * Add suffix option.
    * Send debug output to stderr.
    * Measure by actual time elapsed.
    * Work around broken AnyEvent->timer "period" option.
    * Report aggregates for any metrics seen since startup, even if no events were received during a period (i.e. rate is 0).

--- bin/gmetric-aggregator.pl
+++ bin/gmetric-aggregator.pl
@@ -14,6 +14,7 @@ GetOptions(
     'listen-host=i' => \(my $listen_host = '0.0.0.0'),
     'listen-port=i' => \(my $listen_port = 18649),
     'period=i'      => \(my $period = 60),
+    'suffix=s'      => \(my $units_suffix = '/s'),
     'g|debug'       => \(my $debug),
     'help!'         => \(my $help),
 );
@@ -42,25 +43,35 @@ sub handle {
     return unless $listener->recv(my $buf, 1 << 14);
     my @sample = $emitter->parse($buf);
     return unless $allowed_types{ $sample[METRIC_INDEX_TYPE] };
-    $debug && print Data::Dumper->Dump([\@sample], ['sample']);
+    $debug && warn Data::Dumper->Dump([\@sample], ['sample']);
     $metric_aggregates{ $sample[METRIC_INDEX_NAME] } += $sample[METRIC_INDEX_VALUE];
     $metric_templates{ $sample[METRIC_INDEX_NAME] } ||= \@sample;
 }
 my $watcher = AnyEvent->io(fh => $listener, poll => 'r', cb => \&handle);
 
 # periodically aggregate collected samples and re-emit to target gmond
+my %metrics_seen;
+my $last_time = AnyEvent->time;
+my $timer;
 sub aggregator {
-    for my $metric (keys %metric_aggregates) {
+    my $time = AnyEvent->time;
+    my $measured_period = $time - $last_time;
+    $debug && warn "Aggregating at $time ($measured_period elapsed)\n";
+    $metrics_seen{$_} ||= 1 for keys %metric_aggregates;
+    for my $metric (keys %metrics_seen) {
         my @aggregate = @{ $metric_templates{$metric} };
-        $aggregate[METRIC_INDEX_VALUE] = $metric_aggregates{$metric} / $period;
+        $aggregate[METRIC_INDEX_VALUE] = ($metric_aggregates{$metric}||0) / ($measured_period||1);
         $aggregate[METRIC_INDEX_VALUE] = int($aggregate[METRIC_INDEX_VALUE])
             if $aggregate[METRIC_INDEX_TYPE] =~ /int/;
+        $aggregate[METRIC_INDEX_UNITS] .= $units_suffix;
         $aggregate[METRIC_INDEX_TMAX] = $period;
-        $debug && print Data::Dumper->Dump([\@aggregate], ["${metric}_emitted"]);
         $emitter->gsend(@aggregate);
+        $debug && warn Data::Dumper->Dump([\@aggregate], ["${metric}_aggregated"]);
     }
-    %metric_aggregates = %metric_templates = ();
+    %metric_aggregates = ();
+    $last_time = AnyEvent->time;
+    $timer = AnyEvent->timer(after => $period, cb => \&aggregator);
 }
-my $timer = AnyEvent->timer(after => $period, interval => $period, cb => \&aggregator);
+$timer = AnyEvent->timer(after => $period, cb => \&aggregator);
 
 AnyEvent->condvar->recv;
